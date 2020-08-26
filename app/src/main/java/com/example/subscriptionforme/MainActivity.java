ce1495826572a5a7e00d500041fcdba340796bd0
taesunpark <tovbskvb@daum.net>
2020-08-27 02:56:23 +0900

    [add] 하단바 설정 완료

--- app/src/main/java/com/example/subscriptionforme/MainActivity.java
+++ app/src/main/java/com/example/subscriptionforme/MainActivity.java
@@ -1,14 +1,70 @@
 package com.example.subscriptionforme;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // FrameLayout에 각 메뉴의 Fragment를 바꿔 줌
+    private FragmentManager fragmentManager = getSupportFragmentManager();
+    // 4개의 메뉴에 들어갈 Fragment들
+    private FragmentHome fragmentHome = new FragmentHome();
+    private FragmentRecommend fragmentRecommend = new FragmentRecommend();
+    private FragmentCollectionView fragmentCollectionView = new FragmentCollectionView();
+    private FragmentTransaction transaction;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation_view);
+        // 첫 화면 지정
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.frame_container, fragmentHome).commitAllowingStateLoss();
+        bottomNavigationView.setOnNavigationItemSelectedListener(new ItemSelectedListener());
     }
+
+    class ItemSelectedListener implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            transaction = fragmentManager.beginTransaction();
+
+            switch (menuItem.getItemId()) {
+
+                case R.id.home:
+                    replaceFragment(fragmentHome);
+                    break;
+
+                case R.id.recommend:
+                    replaceFragment(fragmentRecommend);
+                    break;
+
+                case R.id.collection:
+                    replaceFragment(fragmentCollectionView);
+                    break;
+
+            }
+            return true;
+        }
+
+    }
+
+    //fragment 전환하는 메소드
+    public void replaceFragment(Fragment fragment){
+        transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.frame_container, fragment).commit();
+    }
+
 }
