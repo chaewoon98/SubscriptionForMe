3ff537c23b39159a49e26cee46a1cf2854481b5b
taesunpark <tovbskvb@daum.net>
2020-08-27 20:56:40 +0900

    [add] 모아보기 ExpandableListView 추가

--- app/src/main/java/com/example/subscriptionforme/collection/FragmentCollectionView.java
+++ app/src/main/java/com/example/subscriptionforme/collection/FragmentCollectionView.java
@@ -1,20 +1,131 @@
 package com.example.subscriptionforme.collection;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ExpandableListAdapter;
+import android.widget.ExpandableListView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.subscriptionforme.R;
+import com.example.subscriptionforme.main.MainActivity;
+
+import java.security.acl.Group;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class FragmentCollectionView extends Fragment {
+
+    ExpandableListView expandableListView;
+    CustomAdapter adapter;
+    List<String> listGroup;
+    HashMap<String, List<String>> listItem;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
         return inflater.inflate(R.layout.fragment_collectionview, container, false);
     }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState){
+        super.onActivityCreated(savedInstanceState);
+        expandableListView = getActivity().findViewById(R.id.expandable_listview);
+        listGroup = new ArrayList<>();
+        listItem = new HashMap<>();
+        adapter = new CustomAdapter(getContext(), listGroup,listItem);
+        expandableListView.setIndicatorBounds(950,0); // 화살표 위치
+        expandableListView.setAdapter(adapter);
+        initListData();
+        expandableListView.setOnChildClickListener(new GroupClick());
+    }
+
+    public void initListData(){
+        listGroup.add(getString(R.string.group1));
+        listGroup.add(getString(R.string.group2));
+        listGroup.add(getString(R.string.group3));
+        listGroup.add(getString(R.string.group4));
+        listGroup.add(getString(R.string.group5));
+        listGroup.add(getString(R.string.group6));
+        listGroup.add(getString(R.string.group7));
+
+        String[] array;
+
+        List<String> list1 = new ArrayList<>();
+        array = getResources().getStringArray(R.array.group1);
+
+        for(String item : array){
+            list1.add(item);
+        }
+
+        List<String> list2 = new ArrayList<>();
+        array = getResources().getStringArray(R.array.group2);
+
+        for(String item : array){
+            list2.add(item);
+        }
+
+        List<String> list3 = new ArrayList<>();
+        array = getResources().getStringArray(R.array.group3);
+
+        for(String item : array){
+            list3.add(item);
+        }
+
+        List<String> list4 = new ArrayList<>();
+        array = getResources().getStringArray(R.array.group4);
+
+        for(String item : array){
+            list4.add(item);
+        }
+
+        List<String> list5 = new ArrayList<>();
+        array = getResources().getStringArray(R.array.group5);
+
+        for(String item : array){
+            list5.add(item);
+        }
+
+        List<String> list6 = new ArrayList<>();
+        array = getResources().getStringArray(R.array.group6);
+
+        for(String item : array){
+            list6.add(item);
+        }
+
+        List<String> list7 = new ArrayList<>();
+        array = getResources().getStringArray(R.array.group7);
+
+        for(String item : array){
+            list7.add(item);
+        }
+
+        listItem.put(listGroup.get(0),list1);
+        listItem.put(listGroup.get(1),list2);
+        listItem.put(listGroup.get(2),list3);
+        listItem.put(listGroup.get(3),list4);
+        listItem.put(listGroup.get(4),list5);
+        listItem.put(listGroup.get(5),list6);
+        listItem.put(listGroup.get(6),list7);
+
+    }
+
+    public class GroupClick implements ExpandableListView.OnChildClickListener {
+
+        @Override
+        public boolean onChildClick(ExpandableListView expandableListView, View view, int groupPosition, int childPosition, long id) {
+
+            Log.d("박태순",String.valueOf(groupPosition));
+            return false;
+        }
+    }
+
 }
