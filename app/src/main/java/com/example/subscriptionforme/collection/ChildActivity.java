9643132ef9ebfddd5f72b2cc0b52b57dbcb319ee
taesunpark <tovbskvb@daum.net>
2020-08-28 16:07:33 +0900

    [add] 디테일 콜렉션 리얼플루트 추가

--- app/src/main/java/com/example/subscriptionforme/collection/ChildActivity.java
+++ app/src/main/java/com/example/subscriptionforme/collection/ChildActivity.java
@@ -3,6 +3,7 @@ package com.example.subscriptionforme.collection;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -12,11 +13,7 @@ import com.example.subscriptionforme.R;
 
 public class ChildActivity extends AppCompatActivity {
 
-    TextView collectionTitle;
-    TextView collectionDescription;
-    TextView collectionName;
-    TextView collectionPrice;
-    ImageView collectionImage;
+
 
 
     @Override
@@ -26,9 +23,35 @@ public class ChildActivity extends AppCompatActivity {
 
         Intent secondIntent = getIntent();
 
-        String group = String.valueOf(secondIntent.getStringExtra("group"));
-        String childIndex = String.valueOf(secondIntent.getStringExtra("child"));
+        TextView collectionTitle = findViewById(R.id.collectionTitle);
+        TextView collectionDescription = findViewById(R.id.collectionDescription);
+        TextView collectionName = findViewById(R.id.collectionName);
+        TextView collectionPrice = findViewById(R.id.collectionPrice);
+        ImageView collectionImage = findViewById(R.id.collectionImage);
 
+        if(secondIntent.getStringExtra("group").equals("식당")){
+
+        } else if(secondIntent.getStringExtra("group").equals("문화")){
+
+        } else if(secondIntent.getStringExtra("group").equals("카페, 디저트")){
+
+        } else if(secondIntent.getStringExtra("group").equals("식품 배송")){
+            Log.d("박태순","hi");
+            if(secondIntent.getIntExtra("child",0) == 0){
+                collectionDescription.setText("맛있고 신선한 제철과일을 배달 즐기세요!");
+                collectionPrice.setText("24,000원");
+                collectionTitle.setText("REAL Fruit");
+                collectionImage.setImageResource(R.drawable.fruit);
+                Log.d("박태순","hi");
+            }
+
+        } else if(secondIntent.getStringExtra("group").equals("의류")){
+
+        } else if(secondIntent.getStringExtra("group").equals("생활")){
+
+        } else if(secondIntent.getStringExtra("group").equals("기타")){
+
+        }
 
     }
 }
