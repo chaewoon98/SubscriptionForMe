f2c699c348a4858824dfa25fa340a858bd64355b
taesunpark <tovbskvb@daum.net>
2020-09-03 04:13:07 +0900

    [add] 카드사 선택하기 추가

--- app/src/main/java/com/example/subscriptionforme/setting/SettingActivity.java
+++ app/src/main/java/com/example/subscriptionforme/setting/SettingActivity.java
@@ -1,18 +1,40 @@
 package com.example.subscriptionforme.setting;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import com.example.subscriptionforme.R;
+import com.example.subscriptionforme.setting.card.CardActivity;
 
 public class SettingActivity extends AppCompatActivity {
+
+    Button cardDataButton;
 
     @Override
     public void onCreate(@NonNull Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_setting);
+    }
+
+    @Override
+    public void onStart() {
+
+        super.onStart();
+        cardDataButton = findViewById(R.id.buttonCardData);
+        cardDataButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent  = new Intent(view.getContext(), CardActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
---
+++ app/src/main/java/com/example/subscriptionforme/setting/card/CardActivity.java
@@ -0,0 +1,38 @@
+package com.example.subscriptionforme.setting.card;
+
+import android.os.Bundle;
+import android.widget.GridView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import com.example.subscriptionforme.R;
+
+import java.util.ArrayList;
+
+public class CardActivity extends AppCompatActivity {
+
+    GridView gridView;
+    GridAdapter adapter;
+    ArrayList<Card> cards = new ArrayList<>();
+
+    @Override
+    public void onCreate(@NonNull Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_card_company);
+        cards.add(new Card("신한카드",R.drawable.sinhan_logo));
+        cards.add(new Card("현대카드",R.drawable.hyundae_logo));
+        cards.add(new Card("삼성카드",R.drawable.sansung_logo));
+        cards.add(new Card("KB국민카드",R.drawable.kb_logo));
+        cards.add(new Card("롯데카드",R.drawable.lotte_logo));
+        cards.add(new Card("하나카드",R.drawable.hana_logo));
+        cards.add(new Card("우리카드",R.drawable.uri_logo));
+        cards.add(new Card("NH농협카드",R.drawable.nh_logo));
+        cards.add(new Card("BC카드",R.drawable.bc_logo));
+        adapter = new GridAdapter(this, cards);
+        gridView = (GridView)findViewById(R.id.cardGrid);
+        gridView.setAdapter(adapter);
+    }
+
+}
